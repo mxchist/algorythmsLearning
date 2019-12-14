36f0f3bfaaba9f8db2cd65369fbb628b8f9c8927
Max Chistyakov <ilovetolive@inbox.ru>
2019-12-14 19:27:29 +0300

    created new file

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -1,3 +1,20 @@
+import java.io.*;
+
 public class Main {
-    
+
+    public static void main (String ... args) {
+        File inputFile = new File ("INPUT.TXT");
+        try {
+            if (!inputFile.exists()) inputFile.createNewFile();
+        }
+        catch (IOException ioe ) {
+            ioe.printStackTrace();
+        }
+        try {
+            FileReader input = new FileReader("INPUT.TXT");
+        }
+        catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+    }
 }
