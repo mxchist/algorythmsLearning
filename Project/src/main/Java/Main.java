cfcc5c8b6dffd7304a75dde32177e67c5970f4d7
Max Chistyakov <ilovetolive@inbox.ru>
2019-12-24 21:10:48 +0300

    Bubble sort released

--- Project/src/main/Java/Main.java
+++ Project/src/main/Java/Main.java
@@ -1,20 +1,13 @@
-import java.util.Random;
-import java.util.Arrays;
-
 public class Main {
 
     public static void main (String ... args) {
         int arrayLength = 1000000;
-        int[] array1 = new int[arrayLength];
-        int[] array2 = new int[arrayLength];
-        int[] array3 = new int[arrayLength];
-        Random rand = new Random();
+        MyArr array1 = new MyArr(arrayLength);
+        MyArr array2 = new MyArr(arrayLength);
+        MyArr array3 = new MyArr(arrayLength);
 
-        for (int i = 0; i < arrayLength; i++) {
-            array1[i] = rand.nextInt();
-            array2[i] = rand.nextInt();
-            array3[i] = rand.nextInt();
-        }
-
+        array1.showArray();
+        array1.bubbleSort();
+        array1.showArray();
     }
 }
