e601ae9da2228b257fd145700d399615efd2ec41
Max Chistyakov <ilovetolive@inbox.ru>
2020-01-02 12:10:17 +0300

    Main class done; Excercise 2 done

--- Project/src/main/Java/Main.java
+++ Project/src/main/Java/Main.java
@@ -1,11 +1,26 @@
 import java.lang.System;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String ... args) {
-        for (int i = 0; i < 32; i++) {
-            int y = new Random().nextInt(24) + 97;
-            System.out.print((char)y);
+        System.out.println("Введите строку:");
+        char[] cArray;
+//        Scanner sc = new Scanner(System.in);
+//        sc.useDelimiter("\n");
+//        cArray = sc.next().toCharArray();
+        String sc = "Сегодня подобные испытания запрещены, к тому же у большинства владеющих ядерным оружием стран на орбите находится множество спутников, многие из которых будут повреждены достаточно мощным взрывом";
+        System.out.println(sc);
+        cArray = sc.toCharArray();
+        MyStack myStack = new MyStack(10);
+
+        for (int i = 0; i < cArray.length; i++) {
+            myStack.insert(cArray[i]);
+        }
+
+        System.out.printf("%n%1s%n", "Результат:");
+        while (!myStack.isEmpty()) {
+            System.out.print(myStack.remove());
         }
     }
 }
