5e80e4dcd485507dac28ab24e52f3cbce8d4ab99
mchist <maxim.chistyakov@devim.team>
2020-01-09 12:41:05 +0300

    Exercise 3: Main class changed

--- Project/src/main/Java/Main.java
+++ Project/src/main/Java/Main.java
@@ -23,5 +23,35 @@ public class Main {
         while (!myStack.isEmpty()) {
             System.out.print(myStack.pop());
         }
+        System.out.println();
+
+        // Exercise 3
+        System.out.printf("%1s %n", "Задание 3:");
+        String s3 = "Тем временем, разбрасывая во все стороны камешки, Снежок невозмутимо спускался по своему личному оползню. Первые камни ударились о дно каньона всего лишь через мгновение после того, как туда проскакала замыкающая табун лошадь.\n" +
+                "Одеревеневший от ужаса и тряски Ринсвинд вгляделся вперед. Каньон заканчивался тупиком, то есть очередной отвесной стеной.\n" +
+                "А камни все падали и падали, образуя подобие грубой стены. И когда скатился последний валун, Снежок грациозно приземлился прямо на него.\n" +
+                "После чего посмотрел вниз — на запертый в каньоне, сбившийся в кучу табун. Ноздри у Снежка раздувались. Ринсвинд был почти уверен, что лошади не умеют хихикать, но Снежок, похоже, надрывался со смеху.\n";
+        cArray = s3.toCharArray();
+        MyDeck myDeck = new MyDeck<Character>(11);
+        boolean isLeftWasLast = true;
+
+        for (int i = cArray.length-1; i >=0; i--) {
+            myDeck.insertLeft(cArray[i]);
+            i--;
+            isLeftWasLast = true;
+            if (i < cArray.length) {
+                myDeck.insertRight(cArray[i]);
+                isLeftWasLast = false;
+
+            }
+        }
+
+        if (!myDeck.isEmpty() && isLeftWasLast == true) {
+            System.out.print(myDeck.removeLeft());
+        }
+        while (!myDeck.isEmpty()) {
+            System.out.print(myDeck.removeRight());
+            if (!myDeck.isEmpty()) System.out.print(myDeck.removeLeft());
+        }
     }
 }
