5d6ad6ea7567d05d2d228d8b85f7e160643b5c6b
Max Chistyakov <ilovetolive@inbox.ru>
2020-01-23 19:02:54 +0300

    Homework done

--- Project/src/main/Java/Main.java
+++ Project/src/main/Java/Main.java
@@ -2,14 +2,25 @@ import java.util.Random;
 class Main {
 
     public static void main(String ... args) {
-        MyTreeMap<Integer, String> myTreeMap = new MyTreeMap<Integer, String>();
-        Random rand = new Random();
-        do {
-            int ni = rand.nextInt(201) - 101;
-            if (myTreeMap.contains(ni)) continue;
-            myTreeMap.put(ni, "newString");
+        float balancedQty = 0, disBalancedQty = 0;
+        for (int i = 0; i < 20; i ++) {
+            MyTreeMap<Integer, String> myTreeMap = new MyTreeMap<Integer, String>();
+            Random rand = new Random();
+            do {
+                int ni = rand.nextInt(201) - 101;
+                if (myTreeMap.contains(ni)) continue;
+                myTreeMap.put(ni, "newString");
+            }
+            while (myTreeMap.getLevel() < 6);
+
+            // Задание 2
+            balancedQty += myTreeMap.isBalanced() ? 1 : 0;
+            disBalancedQty += myTreeMap.isBalanced() ? 0 : 1;
         }
-        while (myTreeMap.getLevel() < 6);
-        System.out.println(myTreeMap.getLevel());
+
+        // Задание 2
+        System.out.printf("Несбалансированными являются $1%.3f процентов деревьев %n", (disBalancedQty / (balancedQty + disBalancedQty)) / 100f);
+
     }
+
 }
