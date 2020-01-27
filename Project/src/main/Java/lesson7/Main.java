c751c532262b34d44ab3502962c6cc660c347b25
Max Chistyakov <ilovetolive@inbox.ru>
2020-01-27 22:17:40 +0300

    create graphs with 10 vertex

--- Project/src/main/Java/lesson7/Main.java
+++ Project/src/main/Java/lesson7/Main.java
@@ -4,14 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(10);
 
-        graph.addEdge(1,2);
-        graph.addEdge(0,4);
-        graph.addEdge(1,4);
+        graph.addEdge(0,2);
+        graph.addEdge(2,6);
+        graph.addEdge(6,3);
         graph.addEdge(3,4);
+        graph.addEdge(4,7);
         graph.addEdge(7,8);
-        graph.addEdge(1,3);
+        graph.addEdge(8,9);
 
-        System.out.println(graph.getAdjList(1));
+        graph.addEdge(0,3);
+        graph.addEdge(3,7);
+
+        graph.addEdge(0,6);
+        graph.addEdge(6,1);
+        graph.addEdge(1,5);
+        graph.addEdge(5,9);
+
+        System.out.println(graph.getAdjList(0));
         System.out.println(graph.getEdgeCount());
 
 //        DepthFirstPath dfp = new DepthFirstPath(graph, 2);
@@ -20,9 +29,8 @@ public class Main {
 //        System.out.println(dfp.pathTo(4));
 
         BreadthFirstPath bfp = new BreadthFirstPath(graph, 2);
-        System.out.println(bfp.hasPathTo(4));
-        System.out.println(bfp.hasPathTo(8));
-        System.out.println(bfp.pathTo(3));
+        System.out.println(bfp.hasPathTo(9));
+        System.out.println(bfp.pathTo(9));
 
 
     }
